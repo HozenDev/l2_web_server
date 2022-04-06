bf5bdb72b7399803a856e0707adab6eb034950cb
Enzo Durel <enzo.durel@etu.uca.fr>
2022-04-06 10:06:40 +0200

    j'ai fini les getters et setters je suis trop heureux

--- server/src/com/uca/entity/GommetteAttribueeEntity.java
+++ server/src/com/uca/entity/GommetteAttribueeEntity.java
@@ -13,4 +13,54 @@ public class GommetteAttribuee {
     }
     
     // Getters //
+
+    public int getId() {
+	return this.id;
+    }
+
+    public int getIdStudent() {
+	return this.idStudent;
+    }
+
+    public int getIdProf() {
+	return this.idProf;
+    }
+
+    public int getIdGommette() {
+	return this.idGommette;
+    }
+
+    public Date getDate() {
+	return this.date;
+    }
+
+    public String getBehavior() {
+	return this.behavior;
+    }
+
+    // Setters //
+
+    public void setId(int id) {
+	this.id = id;
+    }
+
+    public void setIdStudent(int id) {
+	this.idStudent = id;
+    }
+
+    public void setIdProf(int id) {
+	this.idProf = id;
+    }
+
+    public void setIdGommette(int id) {
+	this.idGommette = id;
+    }
+
+    public void setDate(Date date) {
+	this.date = date;
+    }
+
+    public void setBehavior(String behavior) {
+	this.behavior = behavior;
+    }
 }
