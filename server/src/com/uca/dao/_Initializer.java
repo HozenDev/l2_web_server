d9f420c3d1906a860d7779e387ae215876583e03
Votre Nom <thvillepre@math-2001-07l.sciences.lan>
2022-05-04 08:04:02 +0200

    changement DAO initializer pour première connxion

--- server/src/com/uca/dao/_Initializer.java
+++ server/src/com/uca/dao/_Initializer.java
@@ -10,6 +10,19 @@ public class _Initializer {
         try {
             PreparedStatement statement;
 
+
+		 //Ceer si n'existe pas !
+        statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS users (id int primary key auto_increment, statut varchar(100), username varchar(100), password varchar(100), firstname varchar(100), lastname varchar(100)); ");
+	    statement.executeUpdate();
+        statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS professeurs (id int primary key auto_increment, firstname varchar(100), lastname varchar(100)); ");
+	    statement.executeUpdate();
+	    statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS eleves (id int primary key auto_increment, firstname varchar(100), lastname varchar(100)); ");
+	    statement.executeUpdate();
+	    statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS gommettes (id int primary key auto_increment, color varchar(100), description varchar(100)); ");
+	    statement.executeUpdate();
+	    statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS gommettesAttribuees (id int primary key auto_increment, id_student int, id_prof int, id_gommette int, date varchar(100), behavior varchar(100)); ");
+	    statement.executeUpdate();
+
 	    // remove all instance in sql base
 	    statement = connection.prepareStatement("TRUNCATE TABLE users");
 	    statement.executeUpdate();
